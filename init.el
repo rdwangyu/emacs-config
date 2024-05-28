455e841bb38fc383e1d271728d0c64694a9bf747
rdwangyu <rdwangyu@foxmail.com>
2024-05-28 17:23:07 +0800

    add use-package

--- init.el
+++ init.el
@@ -3,9 +3,9 @@
                          ("melpa"  . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
 (package-initialize)
 
-(defun hello-world()
-  (interactive)
-  (message "Hello, world!"))
+(eval-when-compile
+  (require 'use-package))
+
 (electric-pair-mode t)
 (add-hook 'prog-mode-hook #'show-paren-mode)
 (column-number-mode t)
@@ -19,48 +19,32 @@
 (when (display-graphic-p) (toggle-scroll-bar -1))
 (savehist-mode 1)
 (setq display-line-numbers-type 'relative)
-
-(provide 'init)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(melancholy-theme flycheck company ace-window ivy)))
+ '(initial-frame-alist '((fullscreen . maximized)))
+ '(package-selected-packages '(solarized-theme company ace-window)))
+
+(use-package ace-window
+  :ensure t)
+(global-set-key (kbd "M-o") 'ace-window)
+
+(use-package company
+  :ensure t)
+(add-hook 'after-init-hook 'global-company-mode)
+
+(use-package solarized-theme
+  :ensure t)
+(load-theme 'solarized-dark t)
+
+(provide 'init)
+
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
-(ivy-mode)
-(setq ivy-use-virtual-buffers t)
-(setq enable-recursive-minibuffers t)
-;; enable this if you want `swiper' to use it
-;; (setq search-default-mode #'char-fold-to-regexp)
-(global-set-key "\C-s" 'swiper)
-(global-set-key (kbd "C-c C-r") 'ivy-resume)
-(global-set-key (kbd "<f6>") 'ivy-resume)
-(global-set-key (kbd "M-x") 'counsel-M-x)
-(global-set-key (kbd "C-x C-f") 'counsel-find-file)
-(global-set-key (kbd "<f1> f") 'counsel-describe-function)
-(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-(global-set-key (kbd "<f1> o") 'counsel-describe-symbol)
-(global-set-key (kbd "<f1> l") 'counsel-find-library)
-(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
-(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-(global-set-key (kbd "C-c g") 'counsel-git)
-(global-set-key (kbd "C-c j") 'counsel-git-grep)
-(global-set-key (kbd "C-c k") 'counsel-ag)
-(global-set-key (kbd "C-x l") 'counsel-locate)
-(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
-(define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
-
-(global-set-key (kbd "M-o") 'ace-window)
-
-(add-hook 'after-init-hook 'global-company-mode)
-
-(add-hook 'after-init-hook #'global-flycheck-mode)
-
-(load-theme 'solarized-dark t)
