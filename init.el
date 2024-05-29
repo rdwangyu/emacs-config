ae68503c487b8ca375c39a0ed41042e067ebe9c5
rdwangyu <rdwangyu@foxmail.com>
2024-05-29 19:10:04 +0800

    change the theme

--- init.el
+++ init.el
@@ -24,8 +24,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("51ec7bfa54adf5fff5d466248ea6431097f5a18224788d0bd7eb1257a4f7b773" "3e200d49451ec4b8baa068c989e7fba2a97646091fd555eca0ee5a1386d56077" "57a29645c35ae5ce1660d5987d3da5869b048477a7801ce7ab57bfb25ce12d3e" "fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c" "833ddce3314a4e28411edf3c6efde468f6f2616fc31e17a62587d6a9255f4633" "d89e15a34261019eec9072575d8a924185c27d3da64899905f8548cbd9491a36" "00445e6f15d31e9afaa23ed0d765850e9cd5e929be5e8e63b114a3346236c44c" "285d1bf306091644fb49993341e0ad8bafe57130d9981b680c1dbd974475c5c7" "830877f4aab227556548dc0a28bf395d0abe0e3a0ab95455731c9ea5ab5fe4e1" "7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" default))
  '(initial-frame-alist '((fullscreen . maximized)))
- '(package-selected-packages '(popwin solarized-theme company ace-window)))
+ '(package-selected-packages '(popwin company ace-window)))
 
 (use-package ace-window
   :ensure t)
@@ -35,9 +37,7 @@
   :ensure t)
 (add-hook 'after-init-hook 'global-company-mode)
 
-(use-package solarized-theme
-  :ensure t)
-(load-theme 'solarized-dark t)
+(load-theme 'leuven-dark t)
 
 (setq tramp-use-ssh-controlmaster-options nil)
 
@@ -52,6 +52,8 @@
   :ensure t)
 (require 'popwin)
 (popwin-mode 1)
+
+(set-frame-font "Consolas 10" nil t)
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
